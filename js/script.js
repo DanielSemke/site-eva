2d9624df8c29b01cd5fe3c724e3067aec9cafebd
DanielSemke <44940344+DanielSemke@users.noreply.github.com>
2021-02-25 11:49:48 +0100

    Test1

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// Slidder Swiper
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  lazy: true,
+  slidesPerView: 1,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+     type: 'progressbar',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+
+// Nav Menu
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
